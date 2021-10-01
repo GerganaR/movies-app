c2d695492d4a24489937faae7b7a13fd74472f71
unknown <www.g.roshleva7@gmail.com>
2021-10-01 15:23:27 +0300

    Updates

---
+++ precache-manifest.043832d4f1b793e55be4e3e0c3b09064.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdcaa1a3ef1d4a9ef21dda753eee5332",
+    "url": "/movies-app/index.html"
+  },
+  {
+    "revision": "a73b94e742fb950ae81c",
+    "url": "/movies-app/static/css/main.8878ebb5.chunk.css"
+  },
+  {
+    "revision": "a8b9dea63737512022ad",
+    "url": "/movies-app/static/js/2.03b8e3cf.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/movies-app/static/js/2.03b8e3cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a73b94e742fb950ae81c",
+    "url": "/movies-app/static/js/main.58007e6d.chunk.js"
+  },
+  {
+    "revision": "beffc09320df8152f092",
+    "url": "/movies-app/static/js/runtime-main.868138e1.js"
+  }
+]);
