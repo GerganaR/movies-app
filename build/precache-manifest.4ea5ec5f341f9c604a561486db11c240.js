8d04193e8062890addd925c2f2256932e66503dc
unknown <www.g.roshleva7@gmail.com>
2022-02-18 14:52:52 +0200

    updated:commit

---
+++ build/precache-manifest.4ea5ec5f341f9c604a561486db11c240.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd73f25ed68d0db6ce32a5678f4b16fe",
+    "url": "/movies-app/index.html"
+  },
+  {
+    "revision": "2af6a997fd0b33597516",
+    "url": "/movies-app/static/css/main.0de9021e.chunk.css"
+  },
+  {
+    "revision": "a8b9dea63737512022ad",
+    "url": "/movies-app/static/js/2.03b8e3cf.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/movies-app/static/js/2.03b8e3cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2af6a997fd0b33597516",
+    "url": "/movies-app/static/js/main.58007e6d.chunk.js"
+  },
+  {
+    "revision": "beffc09320df8152f092",
+    "url": "/movies-app/static/js/runtime-main.868138e1.js"
+  }
+]);
